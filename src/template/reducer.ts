47648c2de2f0ec6fdc14c982bc79676af385c7ee
Chris Sprance <chrissprance@gmail.com>
2019-09-01 18:25:54 -0400

    Updated deps, switched reducers over to createReducer method

--- src/template/reducer.ts
+++ src/template/reducer.ts
@@ -1,26 +1,18 @@
 export default (
   ClassCase: string
-) => `import { getType } from 'typesafe-actions';
+) => `import { createReducer } from 'typesafe-actions';
 
-import { actions, Types } from './index';
+import { actions } from './index';
 import default${ClassCase}State from './state';
 
-export default (
-  state: Types.${ClassCase}State = default${ClassCase}State,
-  action: Types.${ClassCase}Actions
-): Types.${ClassCase}State => {
-  switch (action.type) {
-    case getType(actions.standardAction):
-      return [...state];
-      
-    case getType(actions.standardActionParams):
-      return state.map(item => ({...item, active: action.payload === item.id}));
-      
-    case getType(actions.asyncAction${ClassCase}.success):
-      return [...action.payload];
-
-    default:
-      return state;
-  }
-};
+export default createReducer(default${ClassCase}State)
+  .handleAction(actions.standardAction, 
+    (state)=>  [...state]
+  )
+  .handleAction(actions.standardActionParams, 
+    (state, action)=>  state.map(item => ({...item, active: action.payload === item.id}))
+  )
+  .handleAction(actions.asyncAction${ClassCase}.success, 
+    (state, action)=>  [...action.payload]
+  )
 `;
